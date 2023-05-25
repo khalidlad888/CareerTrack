e5a314871441c5a8702bf5efa6cd91e95bce02f5
Khalid <khalidlad888@gmail.com>
2023-05-25 17:02:52 +0530

    create a csv file of data to download

--- controllers/students_controller.js
+++ controllers/students_controller.js
@@ -2,46 +2,77 @@
 const Student = require('../models/student');
 const Interview = require('../models/interview');
 
+//importing json2csv package in const
+const CsvParser = require('json2csv').Parser;
+
 //controller function for creating student data
 module.exports.create = async function (req, res) {
-    try {
-        // Create a new student object using the provided form data
-        let student = await Student.create({
-            batch: req.body.batch,
-            name: req.body.name,
-            email: req.body.email,
-            college: req.body.college,
-            status: req.body.status,
-            dsa_score: req.body.dsa_score,
-            webd_score: req.body.webd_score,
-            react_score: req.body.react_score,
-            user: req.user._id
-        });
+  try {
+    // Create a new student object using the provided form data
+    let student = await Student.create({
+      batch: req.body.batch,
+      name: req.body.name,
+      email: req.body.email,
+      college: req.body.college,
+      status: req.body.status,
+      dsa_score: req.body.dsa_score,
+      webd_score: req.body.webd_score,
+      react_score: req.body.react_score,
+      user: req.user._id
+    });
 
-        // Save the student object to the database
-        await student.save();
+    // Save the student object to the database
+    await student.save();
 
-        // res.status(201).json({ message: 'Student created successfully.' });
-        return res.redirect('back');
-    } catch (err) {
-        console.log("Error in creating student", err)
-    };
+    // res.status(201).json({ message: 'Student created successfully.' });
+    return res.redirect('back');
+  } catch (err) {
+    console.log("Error in creating student", err)
+  };
 };
 
 module.exports.destroy = async function (req, res) {
-    try {
-      let student = await Student.findById(req.params.id)
-      if (student.user == req.user.id) {
-  
-        student.deleteOne();
+  try {
+    let student = await Student.findById(req.params.id)
+    if (student.user == req.user.id) {
 
-        await Interview.deleteMany({ student: req.params.id });
+      student.deleteOne();
 
-        return res.redirect("back");
-  
-      }
-    } catch (error) {
-      console.error(error);
+      await Interview.deleteMany({ student: req.params.id });
+
       return res.redirect("back");
+
     }
-  };
+  } catch (error) {
+    console.error(error);
+    return res.redirect("back");
+  }
+};
+
+module.exports.download = async function (req, res) {
+  try {
+    let students = [];
+
+    let studentData = await Student.find({})
+    .populate('interviews', 'company interview_date result')
+
+    studentData.forEach((student) => {
+      const { name, batch, email, college, status, dsa_score, webd_score, react_score, interviews} = student;
+      students.push({ name, batch, email, college, status, dsa_score, webd_score, react_score, interviews});
+    });
+
+    
+    const csvFields = ['Name', 'Batch', 'Email', 'College', 'Status', 'Dsa_score', 'Webd_score', 'React_score', 'Interviews'];
+    const csvParser = new CsvParser({ csvFields });
+    const csvData = csvParser.parse(students);
+
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment; filename=studentsData.csv");
+
+    res.status(200).end(csvData);
+
+
+  } catch (err) {
+    console.log("Error in downloading the data", err);
+  }
+};
--- routes/students.js
+++ routes/students.js
@@ -6,5 +6,6 @@ const studentsController = require('../controllers/students_controller');
 
 router.post('/create',passport.checkAuthentication, studentsController.create);
 router.get('/destroy/:id', passport.checkAuthentication, studentsController.destroy);
+router.get('/download', passport.checkAuthentication, studentsController.download);
 
 module.exports = router;
