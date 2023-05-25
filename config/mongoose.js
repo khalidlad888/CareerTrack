af6b4020b0c577d1107207dc7d2798d87283fd25
Khalid <khalidlad888@gmail.com>
2023-05-25 21:04:29 +0530

    minor correctiom

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://0.0.0.0/careertrack_database');
+mongoose.connect('mongodb://localhost/careertrack_database');
 
 const db = mongoose.connection;
 
